057a8ea25c27a3266b28e849398d609ed40f64dc
MaDcz <helimadie@hotmail.com>
2019-11-22 18:14:06 +0100

    Improved ListProperty item addition. The items can be added to the end and inserted in the middle, API is a little bit more expressive.

--- cpp/include/mad/codegen/listproperty.hpp
+++ cpp/include/mad/codegen/listproperty.hpp
@@ -23,7 +23,13 @@ public:
   {
   }
 
-  ItemNode& ensure(size_t pos) { return ensureItemNode(pos); }
+  ItemNode& addItem();
+
+  void addItem(std::unique_ptr<ItemNode>&& itemNode);
+
+  ItemNode& ensureItem(size_t pos);
+
+  void ensureItem(size_t pos, std::unique_ptr<ItemNode>&& itemNode);
 
   ItemNode& operator[](size_t pos) { return itemNode(pos); }
 
@@ -54,12 +60,62 @@ protected:
     return *itemNode;
   }
 
-  ItemNode& ensureItemNode(size_t pos)
-  {
-    auto& listNode = ensurePropertyNode();
-    while (pos >= listNode.size())
-      listNode.add(std::make_unique<ItemNode>());
+  ItemNode& ensureItemNode(size_t pos, std::unique_ptr<ItemNode>&& itemNode = nullptr);
+};
 
+template <typename TItemNode>
+inline TItemNode& ListProperty<TItemNode>::addItem()
+{
+  return ensureItemNode(size());
+}
+
+template <typename TItemNode>
+inline void ListProperty<TItemNode>::addItem(std::unique_ptr<ItemNode>&& itemNode)
+{
+  ensureItemNode(size(), std::move(itemNode));
+}
+
+template <typename TItemNode>
+inline TItemNode& ListProperty<TItemNode>::ensureItem(size_t pos)
+{
+  return ensureItemNode(pos);
+}
+
+template <typename TItemNode>
+inline void ListProperty<TItemNode>::ensureItem(size_t pos, std::unique_ptr<ItemNode>&& itemNode)
+{
+  ensureItemNode(pos, std::move(itemNode));
+}
+
+template <typename TItemNode>
+inline TItemNode& ListProperty<TItemNode>::ensureItemNode(size_t pos,
+    std::unique_ptr<ItemNode>&& itemNode)
+{
+  auto& listNode = ensurePropertyNode();
+  while (pos > listNode.size())
+    listNode.add(std::make_unique<ItemNode>());
+
+  if (pos == listNode.size())
+  {
+    // The item doesn't exist yet, add new node to the list.
+    if (!itemNode)
+      itemNode.reset(new ItemNode);
+    auto& itemNodeRef = *itemNode.get();
+    listNode.add(std::move(itemNode));
+    return itemNodeRef;
+  }
+  else if (itemNode)
+  {
+    // We have to replace the existing item node with the new one.
+    assert(pos < listNode.size());
+    listNode.erase(pos);
+    auto& itemNodeRef = *itemNode.get();
+    listNode.insert(pos, std::move(itemNode));
+    return itemNodeRef;
+  }
+  else
+  {
+    // Return the existing item node.
     assert(pos < listNode.size());
     auto& generalItemNode = listNode[pos];
     auto itemNode = dynamic_cast<ItemNode*>(&generalItemNode);
@@ -68,6 +124,6 @@ protected:
 
     return *itemNode;
   }
-};
+}
 
 }} // namespace mad::codegen
